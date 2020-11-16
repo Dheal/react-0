55b75731178b510269fadedfc9ce5b413fa0968c
dheal <dlufiga@gmail.com>
2020-11-16 07:10:53 +0700

    pertama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './login.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div className="login">
+        <h2>Register</h2>
+      <div className="box-login">
+        <i class="fas fa-users"></i>
+        <input type="text" placeholder="Nama Lengkap"></input>
+      </div>
+      <div className="box-login">
+        <i class="fas fa-user"></i>
+        <input type="text" placeholder="Nama Akun"></input>
+      </div>
+      <div className="box-login">
+        <i class="fas fa-envelope"></i>
+        <input type="text" placeholder="Email"></input>
+      </div>
+      <div className="box-login">
+        <i class="fas fa-key"></i>
+        <input type="text" placeholder="Password"></input>
+      </div>
+
+      <button type="submit" class="btn-login">Register</button>
+      </div>
+      
+      
+      
     </div>
   );
 }
